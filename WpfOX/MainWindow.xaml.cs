08676bda09efcda12f4b00fb5328840b2a0e434a
Sam Hubner <K038639@eltham-college.org.uk>
2021-05-27 15:48:54 +0100

    Added ability to clear table

--- WpfOX/MainWindow.xaml.cs
+++ WpfOX/MainWindow.xaml.cs
@@ -20,6 +20,8 @@ namespace WpfOX
     /// </summary>
     public partial class MainWindow : Window
     {
+        static int count = 1;
+        OnX g = new OnX();
         public MainWindow()
         {
             InitializeComponent();
@@ -27,113 +29,108 @@ namespace WpfOX
 
         private void Position1_Click(object sender, RoutedEventArgs e)
         {
-            if (Position1.Content == "X")
-            {
-                Position1.Content = "O";
-            }
-            else
-            {
-                Position1.Content = "X";
-            }
-
+            bool result = g.addCounter(0);
+            if (result == true) updateGUI();
         }
 
         private void Position2_Click(object sender, RoutedEventArgs e)
         {
-            if (Position2.Content == "X")
-            {
-                Position2.Content = "O";
-            }
-            else
-            {
-                Position2.Content = "X";
-            }
-
+            bool result = g.addCounter(1);
+            if (result == true) updateGUI();
         }
 
         private void Position3_Click(object sender, RoutedEventArgs e)
         {
-            if (Position3.Content == "X")
-            {
-                Position3.Content = "O";
-            }
-            else
-            {
-                Position3.Content = "X";
-            }
-
+            bool result = g.addCounter(2);
+            if (result == true) updateGUI();
         }
 
         private void Position4_Click(object sender, RoutedEventArgs e)
         {
-            if (Position4.Content == "X")
-            {
-                Position4.Content = "O";
-            }
-            else
-            {
-                Position4.Content = "X";
-            }
+            bool result = g.addCounter(3);
+            if (result == true) updateGUI();
         }
 
         private void Position5_Click(object sender, RoutedEventArgs e)
         {
-            if (Position5.Content == "X")
-            {
-                Position5.Content = "O";
-            }
-            else
-            {
-                Position5.Content = "X";
-            }
+            bool result = g.addCounter(4);
+            if (result == true) updateGUI();
         }
 
         private void Position6_Click(object sender, RoutedEventArgs e)
         {
-            if (Position6.Content == "X")
-            {
-                Position6.Content = "O";
-            }
-            else
-            {
-                Position6.Content = "X";
-            }
+            bool result = g.addCounter(5);
+            if (result == true) updateGUI();
         }
 
         private void Position7_Click(object sender, RoutedEventArgs e)
         {
-            if (Position7.Content == "X")
-            {
-                Position7.Content = "O";
-            }
-            else
-            {
-                Position7.Content = "X";
-            }
+            bool result = g.addCounter(6);
+            if (result == true) updateGUI();
         }
 
         private void Position8_Click(object sender, RoutedEventArgs e)
         {
-            if (Position8.Content == "X")
-            {
-                Position8.Content = "O";
-            }
-            else
-            {
-                Position8.Content = "X";
-            }
+            bool result = g.addCounter(7);
+            if (result == true) updateGUI();
         }
 
         private void Position9_Click(object sender, RoutedEventArgs e)
         {
-            if (Position9.Content == "X")
+            bool result = g.addCounter(8);
+            if (result == true) updateGUI();
+        }
+
+        private void Turn_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (count == 1)
             {
-                Position9.Content = "O";
+                Turn.Text = "O";
             }
             else
             {
-                Position9.Content = "X";
+                Turn.Text = "X";
             }
+        }
+        private void updateGUI()
+        {
+            Button[] bs = { Position1, Position2, Position3, Position4, Position5, Position6, Position7, Position8, Position9 };
+            Board b = g.getBoard();
+            int[] elementsBoard = b.getBoard();
+            //insert turn update here
+
+            for (int x = 0; x < elementsBoard.Length; x++)
+            {
+                if (elementsBoard[x] == 0) bs[x].Content = " ";
+                if (elementsBoard[x] == 1) bs[x].Content = "O";
+                if (elementsBoard[x] == 2) bs[x].Content = "X";
+            }
+            
+
+        }
+
+        private void Position10_Click(object sender, RoutedEventArgs e)
+        {
+            Board b = g.getBoard();
+            int[] elementsBoard = b.getBoard();
+            Button[] bs = { Position1, Position2, Position3, Position4, Position5, Position6, Position7, Position8, Position9 };
+            for (int x = 0; x < elementsBoard.Length; x++)
+            {
+                if (elementsBoard[x] == 0) elementsBoard[x] = 0;
+                if (elementsBoard[x] == 1) elementsBoard[x] = 0;
+                if (elementsBoard[x] == 2) elementsBoard[x] = 0;
+            }
+            updateGUI();
+        }
+
+        private void Position11_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Position12_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
---
+++ WpfOX/OnX.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfOX
+{
+    class OnX
+    {
+        private Board board;
+        private int turn;
+        public OnX()
+        {
+            board = new Board();
+            turn = 1;
+        }
+
+        public bool addCounter(int pos)
+        {
+            bool added = false;
+            added = board.addCounter(pos, turn);
+            if (added == true)
+            {
+                if (turn == 1)
+                {
+                    turn = 2;
+                }
+                else turn = 1;
+            }
+            return added; ;
+        }
+
+        public Board getBoard()
+        {
+            return board;
+        }
+        public int getTurn()
+        {
+            return turn;
+        }
+    }
+}
